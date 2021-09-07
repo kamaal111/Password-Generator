ba0a105e63032d649614ebca9631cc7dcfe78d13
kamaal111 <kamaal.f1@gmail.com>
2021-09-07 22:57:47 +0200

    Preparing to test macOS

--- Tests/EmptyTest.swift
+++ Tests/EmptyTest.swift
@@ -9,13 +9,9 @@ import XCTest
 
 class EmptyTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws { }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func tearDownWithError() throws { }
 
     func testExample() throws {
         XCTAssert(true)
