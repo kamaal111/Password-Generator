438459840a91f79a8669e9c268ebe2ffcad64d0d
Kamaal <kamaal@tranzer.com>
2021-10-05 18:19:54 +0200

    Setting manual provisioning profile for iOS release

---
+++ Scripts/CICD/set_up_xcode_version.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  set_up_xcode_version.sh
+#  Password-Generator
+#
+#  Created by Kamaal Farah on 05/10/2021.
+#  
+
+sudo xcode-select --switch /Applications/Xcode_13.0.app/Contents/Developer
