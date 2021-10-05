02e26fb799765832af32fae58b8737bf09a6a661
kamaal111 <kamaal.f1@gmail.com>
2021-10-05 22:56:15 +0200

    Using scripts to handle steps

---
+++ Scripts/import_signing_certificate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#  import_signing_certificate.sh
+#  Password-Generator
+#
+#  Created by Kamaal M Farah on 05/10/2021.
+#  
+
+set -euo pipefail
+
+security create-keychain -p "" build.keychain
+security list-keychains -s build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p "" build.keychain
+security set-keychain-settings
+security import <(echo $SIGNING_CERTIFICATE_P12_DATA | base64 --decode) \
+                -f pkcs12 \
+                -k build.keychain \
+                -P $SIGNING_CERTIFICATE_PASSWORD \
+                -T /usr/bin/codesign
+security set-key-partition-list -S apple-tool:,apple: -s -k "" build.keychain
