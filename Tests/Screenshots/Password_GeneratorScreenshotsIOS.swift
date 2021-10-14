dac07d1fb4a327de6c4399681c38ded8cd440390
kamaal111 <kamaal.f1@gmail.com>
2021-10-14 22:57:38 +0200

    Making screenshots of saved passwords

--- Tests/Screenshots/Password_GeneratorScreenshotsIOS.swift
+++ Tests/Screenshots/Password_GeneratorScreenshotsIOS.swift
@@ -8,33 +8,43 @@
 import XCTest
 import PGLocale
 
-class Password_GeneratorScreenshotsIOS: XCTestCase {
+final class Password_GeneratorScreenshotsIOS: XCTestCase {
+
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
         continueAfterFailure = false
+
+        app.launchArguments.append("isUITesting")
+        setupSnapshot(app)
+        app.launch()
     }
 
     override func tearDownWithError() throws { }
 
     func testExample() throws {
-        let app = XCUIApplication()
-        app.launchArguments.append("isUITesting")
-        setupSnapshot(app)
-        app.launch()
+        try screenshotFlow()
+    }
 
+    private func screenshotFlow() throws {
+        try homescreenScreenshot()
+        savedPasswordsScreenShot()
+    }
+
+    private func homescreenScreenshot() throws {
         let passwordLengthStepper = app.steppers["password-length-stepper"]
         _ = passwordLengthStepper.waitForExistence(timeout: 20)
 
         try passwordLengthStepper.incrementStepperUntil(value: 32)
 
-        let generatePasswordButton = app.buttons[PGLocale.Keys.GENERATE_BUTTON.localized]
+        let generatePasswordButton = app.buttons[.GENERATE_BUTTON]
         generatePasswordButton.tap()
 
         let passwordLabel = app.staticTexts["password-label"]
         let passwordLabelValue = try XCTUnwrap(passwordLabel.value as? String)
         XCTAssertNotEqual(passwordLabelValue, PGLocale.Keys.PASSWORD_PLACEHOLDER.localized)
 
-        let copyButton = app.buttons[PGLocale.Keys.COPY.localized]
+        let copyButton = app.buttons[.COPY]
         copyButton.tap()
 
         let copyCheckmark = app.images["checkmark-\(PGLocale.Keys.COPY.localized)"]
@@ -44,17 +54,52 @@ class Password_GeneratorScreenshotsIOS: XCTestCase {
         snapshot("home screen")
     }
 
+    private func savedPasswordsScreenShot() {
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone: app.tabBars.buttons.element(boundBy: 1).tap()
+        case .pad:
+            let sidebarToggle = app.buttons["Back"]
+            _ = sidebarToggle.waitForExistence(timeout: 5)
+            sidebarToggle.tap()
+
+            let savedPasswordsbutton = app.buttons[.SAVED_PASSWORDS]
+            _ = savedPasswordsbutton.waitForExistence(timeout: 5)
+            savedPasswordsbutton.tap()
+
+            let passwordHeader = app.staticTexts[.PASSWORDS].firstMatch
+            _ = passwordHeader.waitForExistence(timeout: 5)
+            XCTAssert(passwordHeader.exists)
+
+            let screenSize = UIScreen.main.bounds
+            app.tapCoordinate(x: screenSize.width / 2, y: screenSize.height / 2)
+        default: XCTFail("not implemented")
+        }
+
+        snapshot("saved passwords")
+    }
+
 }
 
-// - TODO: PUT THIS IN A SEPERATE FILE
+extension XCUIElementQuery {
+    subscript(key: PGLocale.Keys) -> XCUIElement {
+        get { self[key.localized] }
+    }
+}
+
 extension XCUIElement {
+    func tapCoordinate(x xCoordinate: Double, y yCoordinate: Double) {
+        let normalized = self.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
+        let coordinate = normalized.withOffset(CGVector(dx: xCoordinate, dy: yCoordinate))
+        coordinate.tap()
+    }
+
     func incrementStepperUntil(value: Int) throws {
         let stepperValue = try XCTUnwrap(self.value as? String)
         let stepperValueInt = try XCTUnwrap(Int(stepperValue))
 
         let remainder = value - stepperValueInt
         for _ in 0..<remainder {
-            self.incrementStepper()
+            incrementStepper()
         }
 
         XCTAssertEqual(Int(self.value as? String ?? "") ?? 0, value)
