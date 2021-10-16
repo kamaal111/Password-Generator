a33d7214e890299813487a340d857d5a3f7614cd
kamaal111 <kamaal.f1@gmail.com>
2021-10-16 19:19:18 +0200

    Removing sensitive info

---
+++ Scripts/generate-sensetive-content/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	emailAddress := os.Getenv("EMAIL_ADDRESS")
+	emailAddressData := []byte(fmt.Sprintf("%s\n", emailAddress))
+	err := ioutil.WriteFile("fastlane/metadata/review_information/email_address.txt", emailAddressData, 0777)
+	if err != nil {
+		log.Fatalln("could not write to email address file")
+	}
+
+	phoneNumber := os.Getenv("PHONE_NUMBER")
+	phoneNumberData := []byte(fmt.Sprintf("%s\n", phoneNumber))
+	err = ioutil.WriteFile("fastlane/metadata/review_information/phone_number.txt", phoneNumberData, 0777)
+	if err != nil {
+		log.Fatalln("could not write to phone number file")
+	}
+
+	deliverFileContent := fmt.Sprintf(`#  Deliverfile
+	
+	username %s
+	force true
+	automatic_release false
+	sync_screenshots true`, emailAddress)
+	deliverFileContentData := []byte(fmt.Sprintf("%s\n", deliverFileContent))
+	err = ioutil.WriteFile("fastlane/Deliverfile", deliverFileContentData, 0777)
+	if err != nil {
+		log.Fatalln("could not write to phone number file")
+	}
+}
